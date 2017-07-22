14960b4cf92a76f23c33231d13eb9bbe054e1c1d
Javier Gallego Martín <bifuer@gmail.com>
2017-07-22 15:42:50 +0200

    es6 refactor sintax revision some fix

--- index.js
+++ index.js
@@ -15,13 +15,12 @@ require('date.format');
 i18n.load('modelator',__dirname+'/languages/');
 
 var models =module.exports = global.modulesCache.modelator = require('./lib/models');
-
+/*
 for(var i=0,k=Object.keys(models),l=k.length;i<l;i++){
   var key = k[i];
   global[key]=models[key];
 }
-
-console.log(global);
+*/
 
 /*
 
